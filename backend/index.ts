fe8cfeea070a1dc1b738871bca13ca67a56ad091
Marco Molina <marcomolina11@gmail.com>
2025-05-19 15:17:34 -0400

    Initial app setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,20 @@
-import express, { Express } from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import userRouter from "./src/api/user.js";
+import userRouter from './src/api/user.js';
 
 dotenv.config();
 
 const app: Express = express();
-app.use(bodyParser.json({ limit: "5mb" }));
-app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
 app.use(cors());
-app.use("/api/user", userRouter);
+app.use('/api/user', userRouter);
 
-const PORT: string | number = process.env.PORT || 5000;
+const PORT: string | number = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGODB_URL || '')
