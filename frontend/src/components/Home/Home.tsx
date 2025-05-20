3cbbdd818719679e1722efe707d34232561a8a44
Marco Molina <marcomolina11@gmail.com>
2025-05-20 12:21:54 -0400

    Cleanup Home component

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import {
-  Avatar,
-  Button,
-  Container,
-  Grid,
-  Paper,
-  Typography,
-} from '@mui/material';
-import CasinoIcon from '@mui/icons-material/Casino';
-import Input from '../Login/Input';
+import { Container, Paper, Typography } from '@mui/material';
 import { UserData } from '../../types/actionTypes';
-import { styles } from './styles';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../selectors/authSelectors';
 
@@ -30,41 +20,6 @@ const Home: React.FC = () => {
               Login to Play
             </Typography>
           )}
-        </Paper>
-        <Paper sx={styles.paper} elevation={3}>
-          <Avatar sx={styles.avatar}>
-            <CasinoIcon />
-          </Avatar>
-          <Typography variant="h5" color="primary">
-            Lucky 7
-          </Typography>
-          <form style={styles.form} onSubmit={() => {}}>
-            <Grid container spacing={2}>
-              <Typography
-                variant="caption"
-                color="info"
-                sx={styles.typo}
-                align="center"
-              >
-                Set your wager for a chance to win!
-              </Typography>
-              <Input
-                name="wager"
-                label="Your Wager"
-                handleChange={() => {}}
-                type="number"
-              />
-              <Button
-                type="submit"
-                sx={styles.submit}
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Submit Wager
-              </Button>
-            </Grid>
-          </form>
         </Paper>
       </Container>
     </div>
